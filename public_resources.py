3c041a8eed0ad857a23d9633020da74d1e11d6ac
ramRK1 <ramkamra0@gmail.com>
2019-12-27 14:42:00 +0530

    changed public resource file

--- public_resources.py
+++ public_resources.py
@@ -3,11 +3,14 @@ import boto3
 def ec2_snapshots():
     ec2_snap = []
     paginator = client.get_paginator('describe_snapshots')
-    for page in paginator.paginate(RestorableByUserIds=['all'],OwnerIds=[str(owner_id)]):
-        print(page)
-        for image in page['Snapshots']:
-            ec2_snap.append(image['SnapshotId'])
-    return ec2_snap
+    try:
+        for page in paginator.paginate(RestorableByUserIds=['all'],OwnerIds=[str(owner_id)]):
+            print(page)
+            for image in page['Snapshots']:
+                ec2_snap.append(image['SnapshotId'])
+        return ec2_snap
+    except:
+        return []
 
 def ec2_ami():
     ec2_ami = []
@@ -31,18 +34,22 @@ def rds_snapshots():
     rds_snap = []
     client = boto3.client('rds')
     paginator = client.get_paginator('describe_db_instances')
-    for page in paginator.paginate(SnapshotType='public'):
-        for image in page['DBSnapshots']:
-            rds_snap.append(image['DBSnapshotArn'])
-    return rds_snap
+    try:
+        for page in paginator.paginate(SnapshotType='public'):
+            print(page)
+            for image in page['DBSnapshots']:
+                rds_snap.append(image['DBSnapshotArn'])
+        return rds_snap
+    except:
+        return []
 
 
 owner_id = boto3.client('sts').get_caller_identity().get('Account')
 client = boto3.client('ec2')
 public_ec2_ami = ec2_ami()
 public_ec2_snapshots = ec2_snapshots()
-#public_rds_snapshots = rds_snapshots()
+public_rds_snapshots = rds_snapshots()
 
 print(public_ec2_ami)
 print(public_ec2_snapshots)
-#print(public_rds_snapshots)
+print(public_rds_snapshots)
